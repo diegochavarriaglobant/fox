2b50213f3538fee48e1f4b7990f3eb9877f93cef
diego.chavarria <diego.chavarria@globant.com>
2018-01-17 16:06:53 -0600

    Updated unit tests

--- src/main/java/com/fox/platform/contentserv/dom/ent/Feed.java
+++ src/main/java/com/fox/platform/contentserv/dom/ent/Feed.java
@@ -1,5 +1,6 @@
 package com.fox.platform.contentserv.dom.ent;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import lombok.Data;
  *
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Feed {
     private Integer took;
 
--- src/test/java/com/fox/platform/contentserv/infra/serv/EndPointChannelVerticleTest.java
+++ src/test/java/com/fox/platform/contentserv/infra/serv/EndPointChannelVerticleTest.java
@@ -2,21 +2,35 @@ package com.fox.platform.contentserv.infra.serv;
 
 import com.fox.platform.contentserv.cfg.ContentServiceConfig;
 import com.fox.platform.contentserv.cfg.impl.ContentServiceConfigImpl;
+import com.fox.platform.contentserv.dom.ent.Feed;
 import com.fox.platform.lib.cfg.ConfigLibFactory;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
 import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.ServerSocket;
 
+import io.vertx.ext.unit.TestContext;
 import static junit.framework.TestCase.assertTrue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(VertxUnitRunner.class)
 public class EndPointChannelVerticleTest {
 
     private ContentServiceConfig contentServiceConfig;
@@ -24,6 +38,62 @@ public class EndPointChannelVerticleTest {
     private static Logger logger = LoggerFactory.getLogger(EndPointChannelVerticleTest.class);
 
     private static final String URL_DEFAULT_CONFIG = "/default-config.json";
+
+    Vertx vertx;
+    int port;
+
+    @Before
+    public void before(TestContext context) throws IOException {
+        ServerSocket socket = new ServerSocket(8081);
+        port = socket.getLocalPort();
+        socket.close();
+        JsonObject configFile = loadResource(URL_DEFAULT_CONFIG);
+        DeploymentOptions options = new DeploymentOptions()
+                .setConfig(configFile);
+
+        vertx = Vertx.vertx();
+        vertx.deployVerticle(EndPointChannelVerticle.class.getName(), options, context.asyncAssertSuccess());
+        vertx.deployVerticle(ProxyChannelVerticle.class.getName(), options);
+    }
+
+    @After
+    public void after(TestContext context) {
+        vertx.close(context.asyncAssertSuccess());
+    }
+
+    @Test
+    public void callEndPointChannelVerticle(TestContext context) {
+        Async async = context.async();
+        HttpClient client = vertx.createHttpClient();
+        client.getNow(port, "localhost", "/", response -> {
+            response.bodyHandler(body -> {
+                context.assertEquals("<h1>Hello from my first Vert.x 3 application</h1>", body.toString());
+                client.close();
+                async.complete();
+            });
+        });
+    }
+
+    @Test
+    public void callEndPointChannelVerticlePath(TestContext context) {
+        Async async = context.async();
+        WebClient client =  WebClient.create(vertx);
+        client.post(port, "localhost", "/channel?countryId=MX")
+                .ssl(false)
+                .putHeader("Content-Length", "0")
+                .putHeader("Content-Type", "application/json")
+                .putHeader("Cache-Control", "no-cache")
+                .send(response -> {
+            if (response.succeeded()) {
+                HttpResponse<Buffer> data = response.result();
+                context.assertNotNull(data.bodyAsJsonObject());
+            }
+            client.close();
+            async.complete();
+        });
+    }
+
+
 
     @Test
     public void loadConfigFile(){
@@ -41,6 +111,8 @@ public class EndPointChannelVerticleTest {
                 ContentServiceConfigImpl.CONFIG_FIELD,
                 ContentServiceConfigImpl.class
         );
+
+        contentServiceConfig.getAddressProxy();
         assertNotNull(contentServiceConfig);
     }
 
